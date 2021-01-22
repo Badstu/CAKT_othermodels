7fe7e1e1fa1aff602237fec7e36039d7402147a7
sayhi_mac <1045541554@qq.com>
2021-01-22 21:48:41 +0800

    .

--- SAKT/run_sakt.sh
+++ SAKT/run_sakt.sh
@@ -1,1 +1,54 @@
 python -u main1.py --dataset assist2009_updated --num_heads 2 --hidden_units 64 --lr 1e-5
+python -u main1.py --dataset assist2009_updated --num_heads 2 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2009_updated --num_heads 2 --hidden_units 128 --lr 1e-4
+python -u main1.py --dataset assist2009_updated --num_heads 2 --hidden_units 256 --lr 1e-3
+python -u main1.py --dataset assist2009_updated --num_heads 4 --hidden_units 64 --lr 1e-5
+python -u main1.py --dataset assist2009_updated --num_heads 4 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2009_updated --num_heads 4 --hidden_units 256 --lr 1e-4
+python -u main1.py --dataset assist2009_updated --num_heads 8 --hidden_units 64 --lr 1e-4
+python -u main1.py --dataset assist2009_updated --num_heads 8 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2009_updated --num_heads 8 --hidden_units 256 --lr 1e-3
+
+python -u main1.py --dataset assist2015 --num_heads 2 --hidden_units 64 --lr 1e-5
+python -u main1.py --dataset assist2015 --num_heads 2 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2015 --num_heads 2 --hidden_units 128 --lr 1e-4
+python -u main1.py --dataset assist2015 --num_heads 2 --hidden_units 256 --lr 1e-3
+python -u main1.py --dataset assist2015 --num_heads 4 --hidden_units 64 --lr 1e-5
+python -u main1.py --dataset assist2015 --num_heads 4 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2015 --num_heads 4 --hidden_units 256 --lr 1e-4
+python -u main1.py --dataset assist2015 --num_heads 8 --hidden_units 64 --lr 1e-4
+python -u main1.py --dataset assist2015 --num_heads 8 --hidden_units 128 --lr 1e-3
+python -u main1.py --dataset assist2015 --num_heads 8 --hidden_units 256 --lr 1e-3
+
+# python -u main1.py --dataset assist2017 --num_heads 2 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset assist2017 --num_heads 2 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset assist2017 --num_heads 2 --hidden_units 128 --lr 1e-4
+# python -u main1.py --dataset assist2017 --num_heads 2 --hidden_units 256 --lr 1e-3
+# python -u main1.py --dataset assist2017 --num_heads 4 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset assist2017 --num_heads 4 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset assist2017 --num_heads 4 --hidden_units 256 --lr 1e-4
+# python -u main1.py --dataset assist2017 --num_heads 8 --hidden_units 64 --lr 1e-4
+# python -u main1.py --dataset assist2017 --num_heads 8 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset assist2017 --num_heads 8 --hidden_units 256 --lr 1e-3
+
+# python -u main1.py --dataset STATICS --num_heads 2 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset STATICS --num_heads 2 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset STATICS --num_heads 2 --hidden_units 128 --lr 1e-4
+# python -u main1.py --dataset STATICS --num_heads 2 --hidden_units 256 --lr 1e-3
+# python -u main1.py --dataset STATICS --num_heads 4 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset STATICS --num_heads 4 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset STATICS --num_heads 4 --hidden_units 256 --lr 1e-4
+# python -u main1.py --dataset STATICS --num_heads 8 --hidden_units 64 --lr 1e-4
+# python -u main1.py --dataset STATICS --num_heads 8 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset STATICS --num_heads 8 --hidden_units 256 --lr 1e-3
+
+# python -u main1.py --dataset synthetic --num_heads 2 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset synthetic --num_heads 2 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset synthetic --num_heads 2 --hidden_units 128 --lr 1e-4
+# python -u main1.py --dataset synthetic --num_heads 2 --hidden_units 256 --lr 1e-3
+# python -u main1.py --dataset synthetic --num_heads 4 --hidden_units 64 --lr 1e-5
+# python -u main1.py --dataset synthetic --num_heads 4 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset synthetic --num_heads 4 --hidden_units 256 --lr 1e-4
+# python -u main1.py --dataset synthetic --num_heads 8 --hidden_units 64 --lr 1e-4
+# python -u main1.py --dataset synthetic --num_heads 8 --hidden_units 128 --lr 1e-3
+# python -u main1.py --dataset synthetic --num_heads 8 --hidden_units 256 --lr 1e-3
