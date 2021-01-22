14e3845e1a0c52633f0490580fb28b5ca3b8124d
sayhi_mac <1045541554@qq.com>
2021-01-22 16:14:35 +0800

    .

--- DKVMN/run_dkvmn.sh
+++ DKVMN/run_dkvmn.sh
@@ -42,13 +42,13 @@
 # python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
 # python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
 
-python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
-python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
-python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
-python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
-python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
-python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
-python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
-python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
+# python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
+# python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
+# python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
+# python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
+# python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
+# python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
+# python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
+# python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
+# python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
+# python -u main.py --dataset synthetic --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
