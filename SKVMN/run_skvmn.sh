03e16305a58df6ae767443ac4423e3969f78dd51
sayhi_mac <1045541554@qq.com>
2021-01-23 14:49:20 +0800

    .

--- SKVMN/run_skvmn.sh
+++ SKVMN/run_skvmn.sh
@@ -35,11 +35,11 @@ python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 50 --qa_embed
 python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
 python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
 python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
-python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
+python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
 python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
 python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
 python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
+python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-2
 python -u main.py --dataset STATICS --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
 
 # python -u main.py --dataset synthetic --memory_size 20 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
