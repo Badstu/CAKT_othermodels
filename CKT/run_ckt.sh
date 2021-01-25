4ba27dab28ababbc4b70f679de3be11b47b98d34
sayhi_mac <1045541554@qq.com>
2021-01-25 09:53:11 +0800

    .

--- CKT/run_ckt.sh
+++ CKT/run_ckt.sh
@@ -1,13 +1,13 @@
-python -u train.py assist2009_updated 50 0.2 1e-2
-python -u train.py assist2009_updated 50 0.4 1e-3
-python -u train.py assist2009_updated 50 0.4 1e-4
-python -u train.py assist2009_updated 50 0.6 1e-3
-python -u train.py assist2009_updated 100 0.2 1e-2
-python -u train.py assist2009_updated 100 0.4 1e-3
-python -u train.py assist2009_updated 100 0.6 1e-4
-python -u train.py assist2009_updated 150 0.2 1e-4
-python -u train.py assist2009_updated 150 0.4 1e-3
-python -u train.py assist2009_updated 150 0.6 1e-3
+# python -u train.py assist2009_updated 50 0.2 1e-2
+# python -u train.py assist2009_updated 50 0.4 1e-3
+# python -u train.py assist2009_updated 50 0.4 1e-4
+# python -u train.py assist2009_updated 50 0.6 1e-3
+# python -u train.py assist2009_updated 100 0.2 1e-2
+# python -u train.py assist2009_updated 100 0.4 1e-3
+# python -u train.py assist2009_updated 100 0.6 1e-4
+# python -u train.py assist2009_updated 150 0.2 1e-4
+# python -u train.py assist2009_updated 150 0.4 1e-3
+# python -u train.py assist2009_updated 150 0.6 1e-3
 
 # python -u train.py assist2015 50 0.2 1e-2
 # python -u train.py assist2015 50 0.4 1e-3
@@ -42,13 +42,13 @@ python -u train.py assist2009_updated 150 0.6 1e-3
 # python -u train.py STATICS 150 0.4 1e-3
 # python -u train.py STATICS 150 0.6 1e-3
 
-# python -u train.py synthetic 50 0.2 1e-2
-# python -u train.py synthetic 50 0.4 1e-3
-# python -u train.py synthetic 50 0.4 1e-4
-# python -u train.py synthetic 50 0.6 1e-3
-# python -u train.py synthetic 100 0.2 1e-2
-# python -u train.py synthetic 100 0.4 1e-3
-# python -u train.py synthetic 100 0.6 1e-4
-# python -u train.py synthetic 150 0.2 1e-4
-# python -u train.py synthetic 150 0.4 1e-3
-# python -u train.py synthetic 150 0.6 1e-3
+python -u train.py synthetic 50 0.2 1e-2
+python -u train.py synthetic 50 0.4 1e-3
+python -u train.py synthetic 50 0.4 1e-4
+python -u train.py synthetic 50 0.6 1e-3
+python -u train.py synthetic 100 0.2 1e-2
+python -u train.py synthetic 100 0.4 1e-3
+python -u train.py synthetic 100 0.6 1e-4
+python -u train.py synthetic 150 0.2 1e-4
+python -u train.py synthetic 150 0.4 1e-3
+python -u train.py synthetic 150 0.6 1e-3
