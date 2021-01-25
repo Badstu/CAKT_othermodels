25e79e108d10e73434f825988730a40711931ae6
daphnezmx <821611220@qq.com>
2021-01-25 10:10:26 +0800

    add AKT shs

---
+++ AKT/run_param6.sh
@@ -0,0 +1,5 @@
+python -u main.py --dataset assist2009_updated --batch_size 128 --dropout 0.1 --d_model 512 --d_ff 256 --n_head 8
+python -u main.py --dataset assist2015 --batch_size 128 --dropout 0.1 --d_model 512 --d_ff 256 --n_head 8
+python -u main.py --dataset assist2017 --batch_size 32 --dropout 0.1 --d_model 512 --d_ff 256 --n_head 8
+python -u main.py --dataset STATICS --batch_size 32 --dropout 0.1 --d_model 512 --d_ff 256 --n_head 8
+python -u main.py --dataset synthetic --batch_size 128 --dropout 0.1 --d_model 512 --d_ff 256 --n_head 8
