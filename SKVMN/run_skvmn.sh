720dccd2055153a35a83ae4e203296042f05389d
sayhi_mac <1045541554@qq.com>
2021-01-26 11:31:29 +0800

    .

--- SKVMN/run_skvmn.sh
+++ SKVMN/run_skvmn.sh
@@ -14,22 +14,22 @@
 # python -u main.py --dataset assist2015 --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
 # python -u main.py --dataset assist2015 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
 # python -u main.py --dataset assist2015 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
-python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
-python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
-python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
-python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
+# python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
+# python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
+# python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
+# python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
+# python -u main.py --dataset assist2015 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
 
 # python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
 # python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-# python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
-# python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
-# python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
-# python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
-# python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
-# python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
-# python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
-# python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
+python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 50 --qa_embed_dim 200 --init_lr 1e-2
+python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 10 --init_lr 1e-3
+python -u main.py --dataset assist2017 --memory_size 20 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-4
+python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 10 --init_lr 1e-3
+python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
+python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
+python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 50 --init_lr 1e-4
+python -u main.py --dataset assist2017 --memory_size 50 --q_embed_dim 100 --qa_embed_dim 100 --init_lr 1e-2
 
 # python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 50 --qa_embed_dim 50 --init_lr 1e-2
 # python -u main.py --dataset STATICS --memory_size 20 --q_embed_dim 50 --qa_embed_dim 100 --init_lr 1e-3
