3e419f2cd82e9f41253d6c0c03c9f1c0e0f9f5f4
sayhi_mac <1045541554@qq.com>
2021-01-27 17:47:56 +0800

    .

--- EKT/EKT_experiment.py
+++ EKT/EKT_experiment.py
@@ -178,8 +178,8 @@ if __name__ == '__main__':
     list_seq_hidden_size = [50, 100, 150, 50, 100, 100, 150, 50, 100, 150]
     list_text_emb_size = [50, 50, 25, 25, 50, 100, 50, 100, 50, 25]
 
-    for data_source, num_concept in list_datasets[2:3]:
-        for i in range(4, 6):
+    for data_source, num_concept in list_datasets[1:2]:
+        for i in range(8, 10):
             k = list_knowledge_emb_size[i]
             s = list_seq_hidden_size[i]
             t = list_text_emb_size[i]
